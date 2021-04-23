28852a3f020d1fa80ab69795ccf8ccf6d74fd1fd
pelikhan <jhalleux@microsoft.com>
2021-04-23 14:09:39 -0700

    potentiometer firmware

---
+++ targets/keystudio-v1.0/profile/potentiometer.c
@@ -0,0 +1,11 @@
+#include "jdprofile.h"
+#include "jd_drivers.h"
+#include "services/jd_services.h"
+
+// Edit the string below to match your company name, the device name, and hardware revision.
+// Do not change the 0x3.... value, as that would break the firmware update process.
+FIRMWARE_IDENTIFIER(0x3bdb5f6e, "KeyStudio Potentiometer v1.0");
+
+void app_init_services() {
+    potentiometer_init(-1, PIN_X0, -1);
+}
